b9db350847d1e91326a047779fd9ea05fe2e55bb
Ljaewon-123 <jml6388@naver.com>
2025-12-07 15:22:21 +0900

    move service file correct path

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,1 +1,8 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SentimentController;
+
+Route::middleware('auth')->group(function () {
+  Route::post('/sentiment', [SentimentController::class, 'detect'])->name('detect');
+})
